378e245e7a95fb935e4c3dbd41259095f66a4b3d
danteasc4 <zexovon@gmail.com>
2025-02-26 09:29:32 -0500

    All modules added

--- src/data/ScheduleData.ts
+++ src/data/ScheduleData.ts
@@ -19,35 +19,50 @@ export const allData: Schedule = [
       {
         name: 'Intro to p5',
         duration: '15',
-        actType: 'lecalonject',
+        actType: 'lecture',
         links: [
           {
             displayText: 'Lecture',
             linkURL:
               'https://docs.google.com/presentation/d/1UcXrg-FHzM8rlX63F-P0rA-jHUkEPagO34ykHUHBlo0/edit?usp=drive_link',
           },
+        ],
+      },
+      {
+        name: 'Intro to p5',
+        duration: '15',
+        actType: 'codealong',
+        links: [
           {
-            displayText: 'Codealong',
+            displayText: 'Code Along',
             linkURL:
               'https://docs.google.com/document/d/1MS9azdXsl7VcRbZ5dyo5_YFXltoLGI8XLWKhJz_2j1Q/edit?usp=drive_link',
           },
+        ],
+      },
+      {
+        name: 'Intro to p5',
+        duration: '15',
+        actType: 'project',
+        links: [
           {
             displayText: 'Project: Draw that game',
             linkURL:
               'https://docs.google.com/document/d/1eBp-VIwElx4SnjaGv_D6SFiksLJ2amFL5yvY9tQyop8/edit',
           },
+        ],
+      },
+      {
+        name: 'Intro to p5',
+        duration: '15',
+        actType: 'project',
+        links: [
           {
             displayText: 'Optional Project: What would you draw?',
             linkURL:
               'https://docs.google.com/document/d/1AEN6kHpqm0Gz6S_AskQhn6c6eEmW-g0fSDmsG9uYMi4/edit',
           },
         ],
-      },
-      {
-        name: 'Submit your project!',
-        duration: '15',
-        actType: 'survey',
-        links: [{ displayText: 'Submit here', linkURL: 'https://google.com' }],
       },
     ],
   },
@@ -57,30 +72,38 @@ export const allData: Schedule = [
       {
         name: 'Draw Loop + Mouse Events',
         duration: '15',
-        actType: 'lecalonject',
+        actType: 'lecture',
         links: [
           {
             displayText: 'Lecture',
             linkURL:
               'https://docs.google.com/presentation/d/1lotcHQNR_aY4II8N_efojDkBrhmhOTcTmWWo79N4JG0/edit',
           },
+        ],
+      },
+      {
+        name: 'Draw Loop + Mouse Events',
+        duration: '15',
+        actType: 'codealong',
+        links: [
           {
-            displayText: 'Codealong',
+            displayText: 'Code Along',
             linkURL:
               'https://docs.google.com/document/d/1p7UY5WI0qizIGFoO35nwCurEYWXXp5TEjYKnyUMUhkA/edit',
-          },
-          {
-            displayText: 'Project: Draw Loop + Mouse Events',
-            linkURL:
-              'https://docs.google.com/document/d/1oDONc2WibX1RBQSKhBTo-ZkYHqsG_seDCuL7mIfEhto/edit',
           },
         ],
       },
       {
-        name: 'Submit your project!',
+        name: 'Draw Loop + Mouse Events',
         duration: '15',
-        actType: 'survey',
-        links: [{ displayText: 'Submit here', linkURL: 'https://google.com' }],
+        actType: 'project',
+        links: [
+          {
+            displayText: 'Project',
+            linkURL:
+              'https://docs.google.com/document/d/1oDONc2WibX1RBQSKhBTo-ZkYHqsG_seDCuL7mIfEhto/edit',
+          },
+        ],
       },
     ],
   },
@@ -90,30 +113,159 @@ export const allData: Schedule = [
       {
         name: 'Animation',
         duration: '15',
-        actType: 'lecalonject',
+        actType: 'lecture',
         links: [
           {
             displayText: 'Lecture',
             linkURL:
               'https://docs.google.com/presentation/d/1kxryepByZifYn0Ak_k7aoeF7H-HiD3tl7xbdbjaCG7E/edit',
           },
+        ],
+      },
+      {
+        name: 'Animation',
+        duration: '15',
+        actType: 'codealong',
+        links: [
           {
-            displayText: 'Codealong',
+            displayText: 'Code Along',
             linkURL:
               'https://docs.google.com/document/d/1PQgp9O-Y4MWpwWHbrGpROIsVw2dxsAMIoYzxltEKCxw/edit',
           },
+        ],
+      },
+      {
+        name: 'Animation',
+        duration: '15',
+        actType: 'project',
+        links: [
           {
-            displayText: 'Project: Animation',
+            displayText: 'Project',
             linkURL:
               'https://docs.google.com/document/d/1vDPFwsX6R4K6EVH-rPGpzNkTKb8WlLnaB9SkhsA_eQ8/edit?tab=t.0',
           },
         ],
       },
+    ],
+  },
+  {
+    title: 'Module 4 - Bouncing Ball',
+    activities: [
       {
-        name: 'Submit your project!',
+        name: 'Bouncing Ball I',
         duration: '15',
-        actType: 'survey',
-        links: [{ displayText: 'Submit here', linkURL: 'https://google.com' }],
+        actType: 'codealong',
+        links: [
+          {
+            displayText: 'Code Along',
+            linkURL:
+              'https://docs.google.com/document/d/13ofhZTiAS_5qpOyrtZ0Y4WKV8Hnj8UB5OJ-TBi9EzYE/edit',
+          },
+        ],
+      },
+      {
+        name: 'Optional Extra Slides: Logical Operators',
+        duration: '15',
+        actType: 'lecture',
+        links: [
+          {
+            displayText: 'Lecture',
+            linkURL:
+              'https://docs.google.com/presentation/d/10YyX02bnnsLPytwHK420ZcEsTS5eGYdaQ34sDjXMHe4/edit',
+          },
+        ],
+      },
+      {
+        name: 'Bouncing Ball II',
+        duration: '15',
+        actType: 'codealong',
+        links: [
+          {
+            displayText: 'Code Along',
+            linkURL:
+              'https://docs.google.com/document/d/1tmjqr3-SQlX6Sev8irNcBL7aa2q_2R1JDx1b5OrtjB8/edit',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Module 5 - Images',
+    activities: [
+      {
+        name: 'Images',
+        duration: '15',
+        actType: 'codealong',
+        links: [
+          {
+            displayText: 'Code Along',
+            linkURL:
+              'https://docs.google.com/document/d/1olve-OBbMYYp2WR3ia8PWbVw1EJPc8ZdGQBz-YvNOGE/edit',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Module 6 - Keyboard Events',
+    activities: [
+      {
+        name: 'Keyboard Events',
+        duration: '15',
+        actType: 'codealong',
+        links: [
+          {
+            displayText: 'Code Along',
+            linkURL:
+              'https://docs.google.com/document/d/1Bosu6Srt1a7QepIAJUUdGEUnguucNPw7TosIA0-BrRk/edit',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Module 7 - Collision Theory',
+    activities: [
+      {
+        name: 'Collision Theory',
+        duration: '15',
+        actType: 'codealong',
+        links: [
+          {
+            displayText: 'Code Along',
+            linkURL:
+              'https://docs.google.com/document/d/1Bj7uFdnxyv4VvwabfVVjZCkzQtWJGpvh_8UtmgeSvgg/edit',
+          },
+        ],
+      },
+      {
+        name: 'Collision Theory',
+        duration: '15',
+        actType: 'project',
+        links: [
+          {
+            displayText: 'Project',
+            linkURL:
+              'https://docs.google.com/document/d/1sJbWo7UYPRfqG7vi5m3BHcKhJxf-jL5xJ37A_SVoBos/edit',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Module 8 - Your p5 Game',
+    activities: [
+      {
+        name: 'Build & Design Your p5 Game',
+        duration: '15',
+        actType: 'project',
+        links: [
+          {
+            displayText: 'Project',
+            linkURL:
+              'https://docs.google.com/document/d/1U5H1_3129Rgn9Nt8qYYb2YabWOx82Ceu5KwmKmLleOY/edit',
+          },
+        ],
       },
     ],
   },
